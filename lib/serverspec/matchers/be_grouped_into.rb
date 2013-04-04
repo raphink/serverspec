2b7c960aa9466bbad50d01ce5eba543d7769231b
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-04-04 11:53:18 +0200

    Fix be_grouped_into with proper variable

--- lib/serverspec/matchers/be_grouped_into.rb
+++ lib/serverspec/matchers/be_grouped_into.rb
@@ -3,7 +3,7 @@ require 'puppet/type/file'
 
 RSpec::Matchers.define :be_grouped_into do |group|
   match do |file|
-    f = Puppet::Type::File.new(:name => f, :group => group)
+    f = Puppet::Type::File.new(:name => file, :group => group)
     f.insync?(f.retrieve)
   end
 end
