43d17c3bb5fd11f7cfe17de50cbde9c577fcba79
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-04-04 11:40:37 +0200

    Fix indent

--- lib/serverspec/matchers/be_installed.rb
+++ lib/serverspec/matchers/be_installed.rb
@@ -3,12 +3,12 @@ require 'puppet/type/package'
 
 RSpec::Matchers.define :be_installed do
   match do |actual|
-     p = Puppet::Type::Package.new(:name => actual)
-     p_real = p.retrieve
-     if p.exists?
-       p.insync?(p_real)
-     else
-       false
-     end
+    p = Puppet::Type::Package.new(:name => actual)
+    p_real = p.retrieve
+    if p.exists?
+      p.insync?(p_real)
+    else
+      false
+    end
   end
 end
