36cd75acc2071f6f84aa9e3195e3a70df9d09515
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-04-04 11:46:52 +0200

    Add more Puppet types to matchers

--- lib/serverspec/matchers/be_grouped_into.rb
+++ lib/serverspec/matchers/be_grouped_into.rb
@@ -1,6 +1,9 @@
+require 'puppet'
+require 'puppet/type/file'
+
 RSpec::Matchers.define :be_grouped_into do |group|
   match do |file|
-    ret = ssh_exec(commands.check_grouped(file, group))
-    ret[:exit_code] == 0
+    f = Puppet::Type::File.new(:name => f, :group => group)
+    f.insync?(f.retrieve)
   end
 end
--- lib/serverspec/matchers/be_linked_to.rb
+++ lib/serverspec/matchers/be_linked_to.rb
@@ -1,6 +1,9 @@
+require 'puppet'
+require 'puppet/type/file'
+
 RSpec::Matchers.define :be_linked_to do |target|
   match do |link|
-    ret = ssh_exec(commands.check_link(link, target))
-    ret[:exit_code] == 0
+    f = Puppet::Type::File.new(:name => link, :ensure => 'link', :target => target)
+    f.insync?(f.retrieve)
   end
 end
