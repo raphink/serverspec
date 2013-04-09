aba413e2a286134b9f9cfbb203000d8db785fe50
Raphaël Pinson <raphael.pinson@camptocamp.com>
2013-04-10 00:01:08 +0200

    Make sure classes and facts are not nil

--- lib/serverspec/backend/mcollective.rb
+++ lib/serverspec/backend/mcollective.rb
@@ -11,10 +11,12 @@ module Serverspec
           example, values = *args
           mc = rpcclient('spec')
           mc.progress = false
-          example.metadata[:classes].each do |c|
+          classes = example.metadata[:classes] || []
+          classes.each do |c|
             mc.class_filter c
           end
-          example.metadata[:facts].each do |f, v|
+          facts = example.metadata[:facts] || []
+          facts.each do |f, v|
             mc.fact_filter f, v
           end
           status = true
