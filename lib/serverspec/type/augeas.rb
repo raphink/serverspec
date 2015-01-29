df2dbd91aff718039f4369d26126704710c40d05
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-01-29 11:53:12 +0100

    Use real Augeas module

--- lib/serverspec/type/augeas.rb
+++ lib/serverspec/type/augeas.rb
@@ -2,7 +2,7 @@ module Serverspec::Type
   class Augeas < Base
     def match(count)
       require 'augeas'
-      @aug ||= Augeas.open
+      @aug ||= ::Augeas.open
       @aug.match(@name).size
     end
   end
